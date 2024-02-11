bca7b28dc380e11f23593e54c828607fd937abe9
Jason Heron <jasonheron@mail.com>
2024-02-11 15:39:38 +0000

    Leaderboard update

--- app/football/Leaderboard.tsx
+++ app/football/Leaderboard.tsx
@@ -16,135 +16,136 @@ export default async function Leaderboard() {
             </div>
 
             <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>1</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/5Tnjv3qYEJMpAX3ex5kRVPgChHzVaSFvN7NEpJtu4XdQ">
-                        <p>5Tnjv3q</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>3275</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>2</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/66kDYpYtuGkaZF2ho64VgEQuAgBPGjY17HtcTtANtNR8">
-                        <p>66kDYpY</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>2915</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>3</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/8TeymQz7HoxvgW7aV4jJNdyGvZLCiKP2DQM6Ug4kDzBB">
-                        <p>8TeymQz</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>2395</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>4</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/3UTDfLgBBsyFML4MuWGPtGkZFvJZCsji37ceViagueg1">
-                        <p>3UTDfLg</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>1170</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>5</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/CwsDQLkSERBEfF6FzQsVv684wcNh7TrCxUWfNXwVkZZM">
-                        <p>CwsDQLk</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>1235</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>6</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/2rz6ijBWcwQ92LdCQd6rqtzXYtferq9D888f38hVzpnL">
-                        <p>2rz6ijB</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>880</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>7</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/2iTDYwsXMHBydsCRrQGgrTj5EKH8UtScoYX3sTFqRLzw">
-                        <p>2iTDYws</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>765</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>8</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/8PcH4zhVR7wuWyhmip1NJqHpMhd9WcfaSNkxyGgD9w88">
-                        <p>8PcH4zh</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>710</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>9</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/8EJDDUeYPRe9daGHRYVduBJKpkEga7geeticsRPfAoYy">
-                        <p>8EJDDUe</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>640</p>
-                </div>
-            </div>
-            <div className='flex items-center font-light'>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>10</p>
-                </div>
-                <div className='w-1/2 flex items-center gap-1'>
-                    <a href="https://solscan.io/address/8zmLMsLsF48TM31NGe2SxdDXLTWdHA7UNipnqM6vjwt5">
-                        <p>8zmLMsL</p>
-                    </a>
-                </div>
-                <div className='w-1/2 grid justify-items-center grid-cols-2'>
-                    <p>625</p>
-                </div>
-            </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/5Tnjv3qYEJMpAX3ex5kRVPgChHzVaSFvN7NEpJtu4XdQ">
+            <p>5Tnjv3q</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>5040</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>2</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/66kDYpYtuGkaZF2ho64VgEQuAgBPGjY17HtcTtANtNR8">
+            <p>66kDYpY</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>4920</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>3</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/8TeymQz7HoxvgW7aV4jJNdyGvZLCiKP2DQM6Ug4kDzBB">
+            <p>8TeymQz</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>3900</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>4</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/CwsDQLkSERBEfF6FzQsVv684wcNh7TrCxUWfNXwVkZZM">
+            <p>CwsDQLk</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1655</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>5</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/2rz6ijBWcwQ92LdCQd6rqtzXYtferq9D888f38hVzpnL">
+            <p>2rz6ijB</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1410</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>6</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/2iTDYwsXMHBydsCRrQGgrTj5EKH8UtScoYX3sTFqRLzw">
+            <p>2iTDYws</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1385</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>7</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/5gAKMk79Jeghfu14CcbsbYfbaRkKTE8eGMxWZDRwYQzA">
+            <p>5gAKMk7</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1255</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>8</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/8PcH4zhVR7wuWyhmip1NJqHpMhd9WcfaSNkxyGgD9w88">
+            <p>8PcH4zh</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1200</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>9</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/8zmLMsLsF48TM31NGe2SxdDXLTWdHA7UNipnqM6vjwt5">
+            <p>8zmLMsL</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1165</p>
+    </div>
+</div>
+<div className='flex items-center font-light'>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>10</p>
+    </div>
+    <div className='w-1/2 flex items-center gap-1'>
+        <a href="https://solscan.io/address/9H7DBiji7NZV4jc9nbZKYAaYp3x1KTHTa95nWCMgUJ3c">
+            <p>9H7DBij</p>
+        </a>
+    </div>
+    <div className='w-1/2 grid justify-items-center grid-cols-2'>
+        <p>1120</p>
+    </div>
+</div>
+
 
                 
             
